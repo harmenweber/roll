ad1ab7a2034e8c7fb30787419daae84a006a9f7d
Harmen Weber <harmen.weber@gmail.com>
2023-09-16 20:36:06 +0200

    feat: add version option

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.addResourceSource
+
 fun main(args: Array<String>) {
-    RollCommand().main(args)
+    RollCommand(loadConfig()).main(args)
 }
+
+private fun loadConfig() = ConfigLoaderBuilder.default()
+    .addResourceSource("/config.yml")
+    .build()
+    .loadConfigOrThrow<Config>()
--- src/main/kotlin/RollCommand.kt
+++ src/main/kotlin/RollCommand.kt
@@ -2,14 +2,19 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.convert
 import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.options.versionOption
 import com.github.ajalt.mordant.terminal.Terminal
 
-class RollCommand :
+class RollCommand(config: Config) :
     CliktCommand(help = "Rolls dice as per your arguments: d6, 1d8, 2d10, 3d20, and so on.") {
     private val dice: List<Dice> by argument(
         name = "dice",
         help = "The dice to roll (e.g. d6, 1d8, 2d10, 3d20)"
     ).convert { Dice.parse(it) }.multiple(required = true)
+
+    init {
+        versionOption(config.version)
+    }
 
     override fun run() {
         print(roll())
